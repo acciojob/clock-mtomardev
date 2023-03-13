d8f4530d455032a697f496adc53c63f8b649de08
mtomardev <120013813+mtomardev@users.noreply.github.com>
2023-03-13 13:27:59 +0530

    first

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+let time = document.getElementById("timer")
+setInterval(()=>{
+    let d = new Date()
+    let hr = d.getHours()
+    let min = d.getMinutes()
+    let sec = d.getSeconds()
+    let ampm = hr < 12 ? "AM" : "PM";
+    let month = d.getMonth()+1
+    let date = d.getDate()
+    let year = d.getFullYear()
+
+    if(hr>12){
+        hr = hr-12
+    }
+    if(hr === 12){
+        hr = 12
+    }
+
+    time.innerHTML = `${month}/${date}/${year} ${hr}:${min}:${sec} ${ampm} `
+},1000)
